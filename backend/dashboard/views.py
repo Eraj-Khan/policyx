937f51e4b9d87314185a7731ff2b894c4c3c7450
Ali Umair-Xloop <AliUmair2303>
2024-01-18 02:56:58 +0500

    API for listing packages and the bid from companies to user and other companies

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from .models import CompanyDashboard,CompanyPackages
-from .serializers import CompanyDashboardSerializer
+from .serializers import CompanyDashboardSerializer,CompanyPackagesSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -27,3 +27,18 @@ def place_package(requests):
         package_place_serializer.save()
         return Response(package_place_serializer.data, status=status.HTTP_201_CREATED)
     return Response(package_place_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def list_all_packages(request):
+    company_dashboard_data = CompanyDashboard.objects.all()
+    company_packages_data = CompanyPackages.objects.all()
+
+    company_dashboard_serializer = CompanyDashboardSerializer(company_dashboard_data, many=True)
+    company_packages_serializer = CompanyPackagesSerializer(company_packages_data, many=True)
+
+    data = {
+            "company_dashboard": company_dashboard_serializer.data,
+            "company_packages": company_packages_serializer.data,
+        }
+
+    return Response(data)
