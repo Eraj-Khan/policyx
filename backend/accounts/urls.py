11b7010c18bf30e7e6bd3425a10b48a4b030d264
NabeelAhmed2211012KHICND <nabeel.ahmed@xloopdigital.com>
2024-01-23 16:53:25 +0500

    updated code for backend

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-# from .views import CompanyUserListCreate
+from .views import CustomUserRegistrationView
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    # path('company_users/', CompanyUserListCreate.as_view(), name='company-user-list-create'),
+    path('api/auth/users/', CustomUserRegistrationView.as_view({'post': 'create'}), name='user-create')
 ]
