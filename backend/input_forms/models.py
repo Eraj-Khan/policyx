6b5193a6a68a90f58645e13faa868ed53936569e
Eraj-Khan <eraj.khan@xloopdigital.com>
2024-01-27 02:46:56 +0500

    Updated views.py and models.py for relation

--- backend/input_forms/models.py
+++ backend/input_forms/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from accounts.models import User
 
 class UserInformation(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_information')
     age = models.IntegerField(null=True)
     gender = models.CharField(max_length=10)
     bmi = models.FloatField()
