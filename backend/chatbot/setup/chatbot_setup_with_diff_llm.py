d1d6a96bcb5141d0e9efc18fad3f1ba05944c5d6
NabeelAhmed2211012KHICND <nabeel.ahmed@xloopdigital.com>
2024-02-15 13:22:51 +0500

    prompt and pdf updated

--- backend/chatbot/setup/chatbot_setup_with_diff_llm.py
+++ backend/chatbot/setup/chatbot_setup_with_diff_llm.py
@@ -2,30 +2,34 @@ import os
 import pickle
 from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter
-# from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_community.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from PyPDF2 import PdfReader
 
-from langchain_community.embeddings import HuggingFaceEmbeddings
-# from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.embeddings import HuggingFaceEmbeddings
+# from langchain import ChatGoogleGenerativeAI
 from langchain.prompts import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
     ChatPromptTemplate,
 )
 import openai
-
-insrunction_prompt =  ''' 
-    You are policyX professional chatbot assistant for insurance buyers seeking guidance on how to operate our platform policyX and some general information about insurance."
+insrunction_prompt =  '''
+    You are policyX professional buisness chatbot assistant for insurance buyers seeking guidance on how to operate our platform policyX and some general information about insurance."
     provide straight answer not that much detail and alway answer in points
-    policyX is an intermediary platform that connects insurance buyers and insurance companies on a single platform. For insurance buyers, we provide an experience where they have to fill up a form containing Age, BMI, gender, income, etc. This form will then be sent to all the registered insurance companies. These different companies will bid their coverage plans, containing information like total coverage and premium charges, which insurance buyers have to pay. This way, the coverage is custom-based on different insurance buyers' information.
+    policyX is an intermediary platform that connects insurance buyers and insurance companies on a single platform. For insurance buyers, we provide an experience where they have to fill up a form containing Age, BMI, gender, income, etc. This form will then be sent to all the registered insurance companies. These different companies will bid their coverage plans, containing information like total coverage and premium charges, which insurance buyers have to pay. This way, the coverage is custom-based on different insurance buyers information.
+
+    Questions you can answer to Insurance buyers:
+    1. You can greet user if user greets like hi. You should respond  hi welcome to policyX how can i help.
+    2. General questions about insurance (any insurance)
+    3. our platform policyX (policyX related context below contain information of policyX)
 
     Below is an example conversation:
     
-    Question: 
+     Insurance buyers Question: 
      What can you tell me about insurance?   
      PolicyX Chatbot: 
      Insurance is a financial product that provides protection against potential financial losses. It works by transferring the risk from an individual or entity to an insurance company in exchange for payment of a premium.
@@ -36,34 +40,29 @@ insrunction_prompt =  '''
      4. Home insurance
     each offering specific coverage options tailored to different needs and risks
     
-    Question: who is adolf hitler?
-    PolicyX Chatbot: Sorry, I can only provide you insurance and our platform policyX related queries. What would you like to know about policyX?
-
-    Question: what do you know about world war 3?
-    PolicyX Chatbot: Sorry, I can only provide you insurance and our platform policyX related queries. What would you like to know about policyX?
-
+    Insurance buyers Question:
     {question}
     PolicyX Chatbot:
 '''
-
 
 # system_template = """End every answer with "Ahoi Maty I love you". Use the following pieces of context to answer the users question. 
 # If you cannot find the answer from the pieces of context, just say that you don't know, don't try to make up an answer.
 # ----------------
 # {context}"""
 system_template = """ You are responsible for providing information only related to our platform policyX and information 
-related to insurance to the insurance buyers. If insurance buyers ask any irrelevant questions or questions that are not related to insurance, 
-respond with: "Sorry, I would only be able to provide insurance and our platform policyX related queries
-Use the following pieces of context to answer the users question. if the following context doesnot contain answer and question is related to
-insurance just think and answer.
+related to insurance to the insurance buyers. 
+If in the given policyX related context contain answer of the question asked then answer the question without hesitation weather it is relevant or not.
+If the following policyX related context doesnot contain answer and question is related to insurance just think and answer. 
+Following policyX related context contain our platform policyX related information. 
 
 ----------------
+policyX related context:
 {context}"""
 
 def save_to_dir_vectorstore_and_chain(pdf_folder_path):
     # Process PDFs
     vectorstore, conversation_chain = get_vectorstore_and_chain(pdf_folder_path)
-
+    print(conversation_chain)
     # Save trained model
     with open("vectorstore.pkl", "wb") as f:
         pickle.dump(vectorstore, f)
@@ -82,9 +81,9 @@ def get_pdf_text(pdf_docs):
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
-        separator="\\n",
-        chunk_size=300,
-        chunk_overlap=100,
+        separator=" \n ",
+        chunk_size=1000,
+        chunk_overlap=300,
         length_function=len
     )
     chunks = text_splitter.split_text(text)
@@ -107,9 +106,10 @@ def get_vectorstore_and_chain(pdf_folder_path):
 
     # Create vector store
     # vectorstore = FAISS.from_texts(texts=text_chunks, embedding=OpenAIEmbeddings())
-    embeddings = HuggingFaceEmbeddings()
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-
+    # embeddings = HuggingFaceEmbeddings()
+    # vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=OpenAIEmbeddings())
+    
 
     llm = ChatOpenAI()
     # llm = ChatGoogleGenerativeAI(model="gemini-pro",convert_system_message_to_human=True,
@@ -138,13 +138,13 @@ def get_vectorstore_and_chain(pdf_folder_path):
     return vectorstore, conversation_chain
 
 if __name__ == "__main__":
-    load_dotenv()
-    openai.api_key= "hello"
+    # load_dotenv()
     # openai.api_key = os.environ.get("OPENAI_API_KEY")
     # GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
     # print("API_KEY",GOOGLE_API_KEY)
     # os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+    # OPENAI_API_KEY = "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"
+    # os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
     pdf_folder_path = "./pdfs"
     vectorstore, conversation_chain = save_to_dir_vectorstore_and_chain(pdf_folder_path)
-
